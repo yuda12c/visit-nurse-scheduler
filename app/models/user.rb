232ee2961046de811c249b039c66bac3c1c327ef
yuda12c <ngsrn.910@gmail.com>
2025-04-06 11:04:59 +0900

    モデルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :schedules
+  has_many :clients, through: :schedules
+end
---
+++ db/migrate/20250406020338_create_clients.rb
@@ -0,0 +1,11 @@
+class CreateClients < ActiveRecord::Migration[7.1]
+  def change
+    create_table :clients do |t|
+
+      t.timestamps
+    end
+  end
+end
+
+
+
